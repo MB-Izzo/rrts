6a88888371ea0e1bfebf2e8f6b60e9d259137a46
Laurent Victorino <lvictorino@monkeymoon.net>
2017-11-16 10:52:28 +0100

    Review step 1 and 2.

--- Assets/Scripts/Time/TimeManager.cs
+++ Assets/Scripts/Time/TimeManager.cs
@@ -2,20 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class TimeManager : MonoBehaviour {
-
+public static class TimeManager
+{
 	public TimedAction CreateTimedAction(float duration)
 	{
-		return new TimedAction (Time.time, duration);
-	}
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		return new TimedAction(Time.time, duration);
 	}
 }
--- Assets/Scripts/Time/TimedAction.cs
+++ Assets/Scripts/Time/TimedAction.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimedAction {
-
-	public float _time_started { get; private set; }
-	public float _duration { get; private set; }
+public class TimedAction
+{
+	public float time_started { get; private set; }
+	public float duration { get; private set; }
 
 	public TimedAction(float time_started, float duration)
 	{
-		_time_started = time_started;
-		_duration = duration;
+		this.time_started = time_started;
+		this.duration = duration;
 	}
 }
