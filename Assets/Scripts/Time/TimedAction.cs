1d31ef53e06796c658d63eec8f5cad2102266f7b
Mathieu Bouchonnet <mathieubouchonnet@gmail.com>
2017-11-16 17:11:35 +0100

    Basic time working

--- Assets/Scripts/Time/TimedAction.cs
+++ Assets/Scripts/Time/TimedAction.cs
@@ -10,7 +10,7 @@ public class TimedAction {
 	public double action_ratio { get; private set; }
 	public double time_progress { get; private set; }
 	public double _delta { get; private set; }
-	private DateTime last_date;
+	private DateTime last_date = DateTime.Now;
 
 	public TimedAction(float duration, DateTime date)
 	{
@@ -21,7 +21,7 @@ public class TimedAction {
 	public void Update(DateTime new_date)
 	{
 		_delta = MsElapsed (new_date, last_date);
-		this.time_progress += _delta/100;
+		this.time_progress += _delta/10;
 		action_ratio = time_progress / duration;
 		last_date = new_date;
 	}
