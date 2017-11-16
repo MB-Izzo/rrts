7bc05d1de048d6df2dec22924701c425ae6c7214
Mathieu Bouchonnet <mathieubouchonnet@gmail.com>
2017-11-16 10:30:50 +0100

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private Camera _mainCam;
+	private float _minFov = 2.5f;
+	private float _maxFov = 5f;
+
+	public float Speed;
+	private float _speed { get { return this.Speed; } set { this.Speed = value; } }
+
+	public float ZoomSensitivity;
+	private float _zoomSensitivity { get { return this.ZoomSensitivity; } set { this.ZoomSensitivity = value; } }
+
+	// Use this for initialization
+	void Start () {
+		_mainCam = Camera.main;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		HandleMovement ();
+		HandleZoom ();
+
+
+
+	}
+
+	private void HandleZoom()
+	{
+		float fov = _mainCam.orthographicSize;
+		fov += Input.GetAxis ("Mouse ScrollWheel") * - _zoomSensitivity;
+		fov = Mathf.Clamp (fov, _minFov, _maxFov);
+		_mainCam.orthographicSize = fov;
+	}
+
+	private void HandleMovement()
+	{
+		Vector2 input;
+		input.x = Input.GetAxisRaw ("Horizontal");
+		input.y = Input.GetAxisRaw ("Vertical");
+		if (Input.GetKey (KeyCode.D) && !LockedRight())
+		{
+			_mainCam.transform.Translate (Vector3.right * _speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.Q) && !LockedLeft())
+		{
+			_mainCam.transform.Translate (Vector3.left * _speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.Z) && !LockedTop())
+		{
+			_mainCam.transform.Translate (Vector3.up * _speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.S) && !LockedBot())
+		{
+			_mainCam.transform.Translate (Vector3.down * _speed * Time.deltaTime);
+		}
+	}
+
+
+	private bool LockedLeft() 
+	{
+		Vector3 p = _mainCam.WorldToViewportPoint(new Vector3(0, 0, _mainCam.nearClipPlane));
+		if (!isZoomed ())
+		{
+			if (p.x >= 0.6f)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			if (p.x >= 0.9f)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+	}
+
+	private bool LockedRight() 
+	{
+		Vector3 p = _mainCam.WorldToViewportPoint(new Vector3(0, 0, _mainCam.nearClipPlane));
+		if (!isZoomed())
+		{
+			if (p.x <= 0.4f)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			if (p.x <= 0.2f)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+	}
+
+	private bool LockedTop()
+	{
+		Vector3 p = _mainCam.WorldToViewportPoint(new Vector3(0, 0, _mainCam.nearClipPlane));
+		Debug.Log (p);
+		if (!isZoomed ())
+		{
+			if (p.y <= 0.15f)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			if (p.y <= -0.4f)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+	}
+
+	private bool LockedBot()
+	{
+		Vector3 p = _mainCam.WorldToViewportPoint(new Vector3(0, 0, _mainCam.nearClipPlane));
+		Debug.Log (p);
+		if (!isZoomed ())
+		{
+			if (p.y >= 0.6f)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			if (p.y >= 0.9f)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+	}
+
+	private bool isZoomed()
+	{
+		if (_mainCam.orthographicSize <= 3f)
+		{
+			return true;
+		} 
+		else
+		{
+			return false;
+		}
+	}
+}
---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour {
+
+	public Transform dest;
+	private Vector2 start;
+	private Vector2 destV;
+	private Vector2 dir;
+
+
+	private float distance;
+	private float time;
+	public float speed; // in unit/hour.
+	private float s;
+
+	// Use this for initialization
+	void Start () {
+		start = new Vector2 (transform.position.x, transform.position.y);
+		destV = new Vector2 (dest.position.x, dest.position.y);
+
+		dir = destV - start;
+		dir.Normalize ();
+
+		distance = Vector2.Distance (start, destV); // 20Km = 2 units.
+		time = distance / speed; // give time in hour.
+		speed = speed * (1 / 3600f); // convert to speed/s (to use with Time.deltaTime)
+
+		float speedInKm = speed * 10;
+		float timeInMinutes = time * 60;
+
+		print ("Plane is moving at: " + speedInKm + "km/h");
+		print ("It has " + distance * 10 + "km to travel");
+		print ("It will take " + timeInMinutes + " minutes");
+		// s = d/t
+		// t = d/s
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(dir * speed * Time.deltaTime);
+
+	}
+}
